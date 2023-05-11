6ea0428f3b533550203c40fd5aee72bc1f02fc6f
BridgeOfRainbow <34211470+BridgeOfRainbow@users.noreply.github.com>
2023-05-11 14:01:36 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>振知健康GPT</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          更多GPT需求,可联系我们
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
